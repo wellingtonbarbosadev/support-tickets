39bfa31e7eb153c72aeca1e18cf28ade771927e3
Wellington Barbosa <huehueb58@gmail.com>
2026-01-12 15:31:48 -0300

    adiciona funcionalidade de inserção de tickets e inicializa banco de dados com dados de exemplo

--- src/controllers/tickets/create.js
+++ src/controllers/tickets/create.js
@@ -1,6 +1,6 @@
 import { randomUUID } from "node:crypto"
 
-export function create({ request, response }) {
+export function create({ request, response, database }) {
   const { equipment, description, user_name } = request.body
 
   const ticket = {
@@ -12,5 +12,8 @@ export function create({ request, response }) {
     created_at: new Date(),
     updated_at: new Date()
   }
-  return response.end(JSON.stringify(ticket))
+
+  database.insert("tickets", ticket)
+
+  return response.writeHead(201).end(JSON.stringify(ticket))
 }
